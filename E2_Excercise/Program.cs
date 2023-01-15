c8e85d99064512aa79b4759bb2fcd332fddc1f73
Elstriker128 <89691298+Elstriker128@users.noreply.github.com>
2023-01-15 21:32:00 +0200

    Add files via upload

---
+++ E2_Excercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace E2_Excercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            Console.Write("Input word: ");
+            string word = Console.ReadLine();
+            string fin = "Duomenys.txt";
+            string punctuation = "[ ,.;:!?()]+";
+            count = FindCount(fin, punctuation, word);
+            Console.WriteLine(count);
+        }
+        public static int FindCount(string fin, string punctuation, string word)
+        {
+            int count = 0;
+            using (StreamReader sr = new StreamReader(fin))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] values = Regex.Split(line, punctuation);
+                    foreach (string value in values)
+                    {
+                        Match match = Regex.Match(value, $"(^|[{punctuation}]+)({word})([{punctuation}]+|$)");
+                        if (match.Groups[2].Value == word)
+                        {
+                            count++;
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
